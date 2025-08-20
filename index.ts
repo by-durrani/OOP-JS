ca73798c65640b023db792e7c067f5cf841f1608
durraniarts <abdurrehmandurrani1215@gmail.com>
2025-08-20 19:38:41 +0500

    composition, abstraction of class and methods, Implements interface, method overriding ie polymorphism

--- index.ts
+++ index.ts
@@ -16,33 +16,48 @@ abstract class Car {
     return this.status;
   }
 
-  area(): number {
-    return 0;
+  // abstract method simply does forces subclass to implement otherwise throws error
+  abstract area(): number;
+  // area(): number {
+  //   return 0;
+  // }
+}
+
+// COMPOSITION
+class Engine {
+  constructor(private type: string, private horsepower: number) {} // the parameters of a constructor can also be used as a property of that class which is mutable as well
+
+  getSpecs() {
+    return `${this.type} - ${this.horsepower}HP`;
   }
 }
 
 // IMPLEMENTS TS keyword to make sure class have all these methods included
-// interface Drivable {
-//   topSpeed?: number;
-//   getBrand: () => string;
-//   drive: () => string;
-//   getname: () => string;
-//   getFullInfo: () => string;
-//   updateStatus: () => void;
-//   Speed: number;
-//   area: () => number;
-// }
+interface Vehicle {
+  topSpeed?: number;
+  getBrand: () => string;
+  drive: () => string;
+  getname: () => string;
+  getFullInfo: () => string;
+  updateStatus: () => void;
+  Speed: number;
+  area: () => number;
+}
 
 // ENCAPSULATION ALSO ABSTRACTION IS IN THIS EXAMPLE
-class Supra extends Car {
-  public name: string = "supra";
-  private readonly engine: string = "2jz";
-  private speed: number = 0;
+class Supra extends Car implements Vehicle {
+  public name: string = "supra"; // can be accessed outside class and mutable as well inside and out
+  private readonly engine: string = "2jz"; // read-only not mutable
+  private speed: number = 0; // can't be accessed out side class and mutable in class as well
+
+  public engineDetails: Engine; // relationship
+
   constructor() {
     super("toyota");
     // this.getname;
     // this.getEngine;
     // console.log(this.getBrand()) // output: toyota
+    this.engineDetails = new Engine("2JZ-GTE", 320); // composition
   }
 
   getname() {
@@ -63,6 +78,11 @@ class Supra extends Car {
     this.status = "The 2Jz is roaring 🔥";
   }
 
+  // method overriding
+  drive(): string {
+    return "The 2Jz is roaring";
+  }
+
   set Speed(value: number) {
     this.speed = value;
   }
@@ -77,7 +97,11 @@ class Supra extends Car {
 }
 
 const car = new Supra();
-// console.log(car.area());
+
+console.log(car.engineDetails);
+
+// console.log(car.drive()); // with method overriding output => The car is not moving
+// console.log(car.drive()); // with method overriding output => The 2Jz is roaring
 
 // const P_Car = new Car(); // Cannot create an instance of an abstract class.
 
